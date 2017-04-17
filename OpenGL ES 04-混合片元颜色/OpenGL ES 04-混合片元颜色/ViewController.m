c8ee251c145912e667c40c69c14d5021c26e7498
Dustin <fenglinyunshi@126.com>
2017-04-17 17:26:03 +0800

    修改完善注释

--- OpenGL ES 04-混合片元颜色/OpenGL ES 04-混合片元颜色/ViewController.m
+++ OpenGL ES 04-混合片元颜色/OpenGL ES 04-混合片元颜色/ViewController.m
@@ -69,6 +69,7 @@ static const SceneVertex vertices[] = {
                                                                        GLKTextureLoaderOriginBottomLeft, nil] error:NULL];//接受一个CGImageRef并创建一个新的包含CGImageRef的像素数据的OpenGL ES纹理缓存，options参数接受一个存储了用于指定GLKTextureLoader怎么解析加载的图像数据的键值对的NSDictionary。可用选项之一是指示GLKTextureLoader为加载的图像生成MIP贴图。
     
     CGImageRef imageRef1 = [[UIImage imageNamed:@"honeybee"] CGImage];
+    //GLKTextureLoaderOriginBottomLeft确保画出的图像与UIKit坐标一致，不颠倒
     self.info1 = [GLKTextureLoader textureWithCGImage:imageRef1 options:[NSDictionary dictionaryWithObjectsAndKeys:
                                                                        [NSNumber numberWithBool:YES],
                                                                        GLKTextureLoaderOriginBottomLeft, nil] error:NULL];
@@ -98,15 +99,17 @@ static const SceneVertex vertices[] = {
      */
     [self.vertexBuffer prepareToDrawWithAttrib:GLKVertexAttribTexCoord0 numberOfCoordinates:2 attribOffset:offsetof(SceneVertex, textureCoords) shouldEnable:YES];
     
-    self.baseEffect.texture2d0.name = self.info.name;//设置baseEffect的texture2d0属性和使用一个新的纹理缓存。GLKTextureInfo类封装了与刚创建的纹理缓存相关的信息，包含他的尺寸、是否包含MIP贴图、OpenGL ES标识符、名字以及用于纹理的OpenGL ES目标等。
+    //设置baseEffect的texture2d0属性和使用一个新的纹理缓存。GLKTextureInfo类封装了与刚创建的纹理缓存相关的信息，包含他的尺寸、是否包含MIP贴图、OpenGL ES标识符、名字以及用于纹理的OpenGL ES目标等。（纹理1）
+    self.baseEffect.texture2d0.name = self.info.name;
     self.baseEffect.texture2d0.target = self.info.target;
     [self.baseEffect prepareToDraw];
     [self.vertexBuffer drawArrayWithMode:GL_TRIANGLE_STRIP startVertexIndex:0 numberOfVertices:4];
     
+    //设置baseEffect的texture2d0属性和使用一个新的纹理缓存。GLKTextureInfo类封装了与刚创建的纹理缓存相关的信息，包含他的尺寸、是否包含MIP贴图、OpenGL ES标识符、名字以及用于纹理的OpenGL ES目标等。（纹理2）
     self.baseEffect.texture2d0.name = self.info1.name;
     self.baseEffect.texture2d0.target = self.info1.target;
     [self.baseEffect prepareToDraw];
-    [self.vertexBuffer drawArrayWithMode:GL_TRIANGLE_STRIP startVertexIndex:4 numberOfVertices:4];
+    [self.vertexBuffer drawArrayWithMode:GL_TRIANGLE_STRIP startVertexIndex:4 numberOfVertices:4];//从第5个顶点开始数4个顶点
 
     
     
